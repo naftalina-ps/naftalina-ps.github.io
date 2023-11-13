166f6e9332dd27a9fd6d04f9f90eb03ca0046816
matt-integrador <115473561+matt-integrador@users.noreply.github.com>
2023-11-13 08:21:18 -0300

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,127 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Qual planeta tem uma atmosfera densa composta principalmente por dióxido de carbono e é conhecido por suas intensas tempestades de poeira?",
+    answer: "Marte.",
+    options: [
+      "Plutão.",
+      "Saturno.",
+      "Marte.",
+      "Netuno."
+    ]
+  },
+    {
+    numb: 2,
+    question: "Qual das seguintes luas não pertence ao planeta Jupiter?",
+    answer: "Deimos.",
+    options: [
+      "Callisto.",
+      "lo",
+      "Europa.",
+      "Deimos."
+    ]
+  },
+    {
+    numb: 3,
+    question: "Qual é a lua que orbita Marte e é uma das duas luas marcianas, conhecida por sua pequena órbita e forma irregular?",
+    answer: "Phobos.",
+    options: [
+      "Selene.",
+      "Phobos.",
+      "Titã.",
+      "lo."
+    ]
+  },
+    {
+    numb: 4,
+    question: "Qual é o planeta-anão localizado na região do Cinturão de Kuiper, conhecido por sua órbita altamente elíptica e por ser o primeiro objeto transnetuniano a ser visitado por uma sonda da Terra?",
+    answer: "Plutão.",
+    options: [
+      "Ceres.",
+      "Eureas.",
+      "Makemake.",
+      "Plutão."
+    ]
+  },
+    {
+    numb: 5,
+    question: "Qual é a constelação conhecida por conter a estrela Polar e é frequentemente utilizada como referência para encontrar o Norte celeste?",
+    answer: "Ursa maior.",
+    options: [
+      "Orion.",
+      "Ursa menor.",
+      "Ursa maior.",
+      "Polar astro."
+    ]
+  },
+  {
+    numb: 6,
+    question: "Qual é a constelação que representa uma mulher encadeada na mitologia grega, conhecida por suas estrelas em forma de 'V' ?",
+    answer: "Andrômeda.",
+    options: [
+      "Lira.",
+      "Andrômeda.",
+      "Cisne.",
+      "Pégaso."
+    ]
+  },
+  {
+    numb: 7,
+    question: "Qual é o cometa famoso que tem uma órbita periódica de aproximadamente 76 anos e foi observado pela última vez em 1986?",
+    answer: "Halley.",
+    options: [
+      "Halley.",
+      "Encke.",
+      "Shoemaker-Levy 9.",
+      "Halle-bop."
+    ]
+  },
+  {
+    numb: 8,
+    question: "O cometa Hale-Bopp ficou famoso por ser um dos cometas mais brilhantes visíveis a olho nu no século XX. Em que ano ele alcançou o seu brilho máximo?",
+    answer: "1997.",
+    options: [
+      "1860.",
+      "1982.",
+      "1978.",
+      "1997."
+    ]
+  },
+  {
+    numb: 9,
+    question: "Qual é a unidade de medida comumente usada para expressar a massa de buracos negros supermassivos, como TON 618?",
+    answer: "Massas Solares.",
+    options: [
+      "Gigaluzes.",
+      "Quilômetros.",
+      "Massas Solares.",
+      "Megatoneladas."
+    ]
+  },
+  {
+    numb: 10,
+    question: "O buraco negro NGC 1600 é conhecido por ser um buraco negro supermassivo devido à sua:",
+    answer: "Localização central em um aglomerado de galáxias.",
+    options: [
+      "Fusão em outra galáxia.",
+      "Intensa emissão de radiação gama.",
+      "Localização central em um aglomerado de galáxias.",
+      "Rápida rotação ao redor do seu eixo."
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
